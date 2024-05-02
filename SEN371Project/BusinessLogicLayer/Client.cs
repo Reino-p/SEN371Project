061a0d713d06b1b02f3d42f32ac1901250a81701
Reino-p <reinopowell61@gmail.com>
2024-05-02 20:09:28 +0200

    Add project files.

---
+++ SEN371Project/BusinessLogicLayer/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//regex
+using System.Text.RegularExpressions;
+
+namespace SEN371Project.BusinessLogicLayer
+{
+    internal class Client
+    {
+        //properties
+        int clientId { get; set; }
+        string clientName { get; set; }
+        string clientNumber { get; set; } //to-do: add some regex for number validation
+        string location { get; set; }
+
+
+        //constructor
+        public Client(int id, string cname, string cnumber, string loc)
+        {
+            this.clientId = id;
+            this.clientName = cname;
+            this.clientNumber = cnumber;
+            this.location = loc;
+        }
+
+        //methods
+        public string addClient()
+        { 
+            //add client to repository
+            //number validation on client number
+        }
+
+        public string updateClient()
+        {
+            //update client in repository
+            //number validation on client number
+        }
+
+        public string getClient()
+        {
+            //get client from repository
+        }
+    }
+}
---
+++ SEN371Project/BusinessLogicLayer/Users.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEN371Project.BusinessLogicLayer
+{
+    internal class Users
+    {
+        //properties
+        string email { get; set; }
+        string password { get; set; }
+        string name { get; set; }
+        string surname { get; set; }
+
+        //constructor
+        public Users(string e, string p, string n, string s)
+        {
+            this.email = e;
+            this.password = p;
+            this.name = n;
+            this.surname = s;
+        }
+
+        //methods
+        public string createUser()
+        {
+            //enter user details into repository
+        }
+
+        public string getUser()
+        {
+            //return user details from repository
+        }
+
+        public string updateUser()
+        {
+            //update user details in the repository
+        }
+
+        public string deleteUser()
+        {
+            //remove the user details from repository
+        }
+    }
+}
